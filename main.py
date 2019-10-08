c73e1300fd6a8b601142b42917d0f6855ccb98e7
finnlimjc <50428524+finnlimjc@users.noreply.github.com>
2019-10-08 23:14:42 +0800

    Added next steps and scaled the dataset.

--- main.py
+++ main.py
@@ -20,7 +20,21 @@ all_weekdays = pd.date_range(start = start_date, end = end_date, freq = 'B') #B 
 closing_prices = closing_prices.reindex(all_weekdays) #To include all missing dates.
 closing_prices = closing_prices.fillna(method = 'ffill') #ffill stands for Forward Fill
 
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+'Left here. Figure out how to scale only values and keep the data format.'
+scaler = StandardScaler()
+closing_prices = scaler.fit_transform(closing_prices)
+
+#Splitting into train/test
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size)
+'Wait what, how do you even split a single column dataset. Maybe use Facebook library'
+
+#Benchmark Accuracy
+'Logistic Regression first then try SVR'
 
 #References
 """
     https://learndatasci.com/tutorials/python-finance-part-yahoo-finance-api-pandas-matplotlib/
+"""
