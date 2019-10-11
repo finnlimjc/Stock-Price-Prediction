53bbb2d54169a26d8d92391cfcd547841ef787ce
finnlimjc <50428524+finnlimjc@users.noreply.github.com>
2019-10-11 22:54:59 +0800

    Cleaned up the code by making various functions to easily prepare more models.

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
+#Simple Functions for Neatness
+def round_to_2dp (target):
+    result = round(target*100, 2)
+    return result
+
 #Retrieving Dataset
 from pandas_datareader import data
+def get_data(ticker, start_date, end_date, source):
+    """
+    ticker = ticker symbol
+    source = source of stock prices. E.g Google Finance, Yahoo Finance, Stooq, Quandl
+    returns the dataset requested.
+    """
+    ticker = ticker
+    start_date = start_date #Only 2015 onwards due to FREE data source. YYYY-MM-DD
+    dataset = data.DataReader(ticker, source, start_date, end_date)
+    return dataset
 ticker = 'AAPL'
-start_date = '2015-01-02' #Only 2015 onwards due to FREE data source. YYYY-MM-DD
+start_date = '2015-01-02'
 end_date = datetime.date.today()
-dataset = data.DataReader(ticker, 'stooq', start_date, end_date)
+dataset = get_data(ticker, start_date, end_date, 'stooq') #Main settings to change
 
 #Cleaning Dataset
 """
@@ -50,34 +65,45 @@ from sklearn.linear_model import LinearRegression
 linear_r = LinearRegression()
 linear_r.fit(x_train, y_train)
 benchmark = linear_r.score(x_test, y_test)
+print("Linear Regression Accuracy: " + str(round_to_2dp(benchmark)) + "%")
 
 #Cross Validation using Linear Regression to Evaluate Model
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn import metrics
-
-def cross_validation (estimator, x_train, y_train, x_test, y_test):
+def cross_validation (estimator, x_train, y_train, x_test, y_test, name):
+    """
+    estimator = model
+    x_train = independent variables from train set.
+    y_train = dependent variables from train set.
+    x_test = independent variables from test set.
+    y_test = dependent variables from test set.
+    name = model's name (For printing.)
+    returns predicted results from test set.
+    """
     accuracies = cross_val_score(estimator = estimator, X = x_train, y = y_train, cv = 20)
-    predict = cross_val_predict(estimator = estimator, X = x_test, y = y_test, cv = 20)
-    predscore = metrics.r2_score(y_true = y_test, y_pred = predict)
-    print("Linear Regression Average Accuracy: " + str(accuracies.mean()) + " (Train Set)")
-    print("Linear Regression Average Accuracy: " + str(predscore) + " (Test Set)")
-    return predict
-
-linear_r_kfold = LinearRegression()
-linear_r_predscore = cross_validation(linear_r_kfold, x_train, y_train, x_test, y_test)
-
-#Printing All Accuracies
-print("Linear Regression Accuracy: " + str(benchmark))
-print("Linear Regression Average Accuracy: " + str(linear_r_predscore))
+    predict_result = cross_val_predict(estimator = estimator, X = x_test, y = y_test, cv = 20)
+    predscore = metrics.r2_score(y_true = y_test, y_pred = predict_result)
+    print(name + " Average Accuracy: " + str(round_to_2dp(accuracies.mean())) + "% (Train Set)")
+    print(name + " Average Accuracy: " + str(round_to_2dp(predscore)) + "% (Test Set)")
+    return predict_result
+linear_r_test = cross_validation(linear_r, x_train, y_train, x_test, y_test, "Linear Regression")
 
 #Predicting Future Prices by n days using Benchmark
 def y_pred(estimator, prepared_dataset):
+    """
+    estimator = model
+    returns future predicted prices.
+    """
     forecast_out = 20
     forecast = prepared_dataset[-forecast_out:].drop(columns = 'Prediction')
     y_pred = estimator.predict(forecast)
     return y_pred
-
+def plotting_data (y_pred, prepared_dataset):
+    """
+    y_pred = future predicted prices
+    returns data for plotting of charts (REQUIRES 3 CONTAINERS)
+    """
     end_date = datetime.date.today()
     tomorrow = end_date + datetime.timedelta(days = 1)
     forecast_out = 20    
@@ -85,22 +111,35 @@ def y_pred(estimator, prepared_dataset):
     future_prices = pd.DataFrame(data = {'Prediction': y_pred.flatten()}, index = future_weekdays)
     final_dataset = prepared_dataset.drop(columns = 'Prediction')
     final_dataset = final_dataset.append(future_prices, sort = True)
-    return final_dataset
+    return final_dataset, future_weekdays, future_prices
 
-linear_regression = y_pred(estimator = linear_r_kfold, prepared_dataset = closing_prices)
+#All Future Predicted Prices
+linear_regression = y_pred(estimator = linear_r, prepared_dataset = closing_prices)
+linear_regression, lr_weekdays, lr_prices = plotting_data(linear_regression, closing_prices)
 
 #Plotting the Next n Days of Stock Price
-plt.plot(all_weekdays, closing_prices['Close'], color = 'blue') #Previous Prices
-plt.plot(future_weekdays, future_prices, color = 'green') #Future Prices
-plt.tick_params(labelsize = '8') #Adjusting the xlabel and ylabel font size.
-plt.title("Line Chart for Closing Prices of " + ticker)
-plt.xlabel('Date')
-plt.ylabel("Closing Price ($)")
-plt.grid()
-plt.show()
+def macro_plot (name, all_weekdays, prepared_dataset, future_weekdays, future_prices, ticker):
+    """
+    name = name of model
+    all_weekdays has been declared above.
+    future_weekdays = the weekdays that indexes the prices we have predicted.
+    future_prices = the predicted prices
+    ticker has been declared above.
+    shows a line chart of both old and new prices.
+    """
+    plt.plot(all_weekdays, prepared_dataset['Close'], color = 'blue') #Previous Prices
+    plt.plot(future_weekdays, future_prices, color = 'green') #Future Prices
+    plt.tick_params(labelsize = '8') #Adjusting the xlabel and ylabel font size.
+    plt.title("Line Chart for Closing Prices of " + ticker)
+    plt.suptitle("Linear Regression", size = 12)
+    plt.xlabel('Date')
+    plt.ylabel("Closing Price ($)")
+    plt.grid()
+    plt.show()
+macro_plot("Linear Regression", all_weekdays, closing_prices, lr_weekdays, lr_prices, ticker)
 
 #Plotting the Prediction Only
-plt.plot(future_weekdays, future_prices, color = 'green') #Future Prices
+plt.plot(lr_weekdays, lr_prices, color = 'green') #Future Prices
 plt.tick_params(labelsize = '6') #Adjusting the xlabel and ylabel font size.
 plt.title("Line Chart for Predicted Prices of " + ticker)
 plt.xlabel('Date')
